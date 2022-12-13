7bfdd04078b1845c56e9e6c8825a4773788df4e6
nithishkk <109412506+nithishkk@users.noreply.github.com>
2022-12-13 07:38:35 -0500

    added form using toggale

--- src/App.js
+++ src/App.js
@@ -5,12 +5,23 @@ export default function App() {
    const[toggale,setToggale] =React.useState(true)
    return (    
       <div>
-     {toggale ?<h1>Hello StackBlitz!</h1> :
-      <p> Take advantage of limited-time savings on select laptops. Shop our exclusive deals and get the best for less.</p>
+     {toggale ?
+     <form>
+       name:<input type='text'/> <br/>
+    password :<input type='password'/> <br/>
+    submit :<input type="submit"/> <br/>
+       </form> :
+    //  <h1>Hello StackBlitz!</h1> :
+    <div>
+      <input type='text'/><br/>
+      <input type='password'/> <br/>
+      <input type='submit'/>
+      </div>
+      // <p> Take advantage of limited-time savings on select laptops. Shop our exclusive deals and get the best for less.</p>
       
      }
 
-       <button onClick={()=>setToggale(!toggale)}>more</button>
+       <button onClick={()=>setToggale(!toggale)}>submit</button>
      </div>
    );
 }
